e51b25ab16133166de4dac72c312181c864f36bb
JUNSEO OH <piker0925@gmail.com>
2026-06-25 20:18:55 +0900

    feat: 초기 프로젝트 세팅

--- back/src/main/java/com/scommit/global/base/BaseEntity.java
+++ back/src/main/java/com/scommit/global/base/BaseEntity.java
@@ -1,4 +1,30 @@
 package com.scommit.global.base;
 
-public class BaseEntity {
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.PROTECTED)
+    private Long id;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime deletedAt;
+
+    public void softDelete() {
+        this.deletedAt = LocalDateTime.now();
+    }
 }
