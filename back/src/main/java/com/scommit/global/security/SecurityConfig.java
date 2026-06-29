0f802518d311480c0f7628eb7733bc4e4e7f35f6
EuniceNam <caellime@gmail.com>
2026-06-29 10:17:25 +0900

    fix: SucurityConfig에 누락된 Configuration 어노테이션 추가

--- back/src/main/java/com/scommit/global/security/SecurityConfig.java
+++ back/src/main/java/com/scommit/global/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.scommit.global.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
@@ -10,6 +11,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
+@Configuration
 public class SecurityConfig {
 
     @Bean
