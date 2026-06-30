9d0cbfccee624e0ab7f977aa72e62a66e90aed0f
EuniceNam <caellime@gmail.com>
2026-06-30 15:49:10 +0900

    feat: 회원가입 API 구현

--- back/src/main/java/com/scommit/domain/user/controller/UserController.java
+++ back/src/main/java/com/scommit/domain/user/controller/UserController.java
@@ -1,9 +1,19 @@
 package com.scommit.domain.user.controller;
 
+import com.scommit.domain.user.dto.SignupRequest;
+import com.scommit.domain.user.dto.SignupResponse;
+import com.scommit.domain.user.entity.User;
 import com.scommit.domain.user.service.UserService;
+import com.scommit.global.dto.RsData;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +22,19 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "UserController", description = "API 유저 컨트롤러")
 public class UserController {
     private final UserService userService;
+
+    @PostMapping("/signup")
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "회원가입", description = "신규 유저가 회원가입합니다.")
+    public RsData<SignupResponse> signUp(
+            @Valid @RequestBody SignupRequest request
+    ) {
+        User user = userService.signUp(request.email(), request.password(), request.nickname());
+        return new RsData<>(
+                "201-1",
+                "회원가입에 성공했습니다.",
+                new SignupResponse(user)
+        );
+    }
 
 }
--- back/src/main/java/com/scommit/domain/user/service/UserService.java
+++ back/src/main/java/com/scommit/domain/user/service/UserService.java
@@ -1,9 +1,14 @@
 package com.scommit.domain.user.service;
 
+import com.scommit.domain.user.entity.User;
+import com.scommit.domain.user.entity.UserRole;
 import com.scommit.domain.user.repository.UserRepository;
+import com.scommit.global.exception.BusinessException;
+import com.scommit.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +16,18 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 
+    @Transactional
+    public User signUp(String email, String password, String nickname) {
+        if (userRepository.existsByEmail(email)) {
+            throw new BusinessException(ErrorCode.DUPLICATE_EMAIL);
+        }
+        String encodedPassword = passwordEncoder.encode(password);
+        User user = User.builder()
+                .email(email)
+                .password(encodedPassword)
+                .nickname(nickname)
+                .role(UserRole.USER)
+                .build();
+        return userRepository.save(user);
+    }
 }
